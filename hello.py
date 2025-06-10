714e5bae1370218b7ed8239549f1a5b32cff7b71
Jun-won <chicksana28@gmail.com>
2025-06-10 14:48:06 +0900

    hello.py

--- hello.py
+++ hello.py
@@ -21,7 +21,7 @@ st.markdown("""
     margin-bottom: 80px;
 }
 .title-box h2 {
-    font-size: 34px;
+    font-size: 40px;
     font-weight: 700;
     line-height: 1.6;
     margin-bottom: 12px;
@@ -83,19 +83,18 @@ st.markdown(f"""
 <div class="page-wrapper">
     <!-- 상단 제목 -->
     <div class="title-box">
-        <h2>내게 꼭 맞는 자동차 통계,<br>어디서부터 살펴봐야 할지 고민되셨나요?</h2>
-        <p>브랜드별 판매 현황부터 전국 등록 통계까지 시각화로 한눈에!</p>
+        <h2>자동차 시장의 모든 데이터를 한 곳에</h2>
+        <p>판매량부터 등록현황까지, 차량 데이터 모음집</p>
     </div>
     <!-- ✅ 브랜드: 이미지 오른쪽 -->
     <!-- 브랜드별 판매 통계: 이미지 오른쪽 / 텍스트 왼쪽 -->
     <div class="section">
         <div class="text-box">
-            <div style="color:#3478f6; font-size:16px; font-weight:600; margin-bottom:10px;">브랜드별 판매 통계</div>
-            <div style="font-size:30px; font-weight:700; line-height:1.5;">
-                브랜드별 판매 실적과<br>
-                점유율 흐름을 한눈에 확인해보세요.
+            <div style="color:#1E3A8A; font-size:35px; font-weight:700; margin-bottom:16px;">🚗어떤 차가 실제로 팔리고 있을까?</div>
+            <div style="font-size:20px; font-weight:700; line-height:1.2;">
+            월별 판매 트렌드 확인하기
             </div>
-            <div style="margin-top:20px;">
+            <div style="margin-top:16px;">
                 <a href="/brand" target="_self" style="display:inline-block; padding:10px 18px; background-color:#3478f6; color:white; border-radius:6px; text-decoration:none; font-weight:600; margin-top:20px;">
                 자세히 보기 →
                 </a>
@@ -111,8 +110,8 @@ st.markdown(f"""
             <img src="data:image/png;base64,{img_base64_2}" />
         </div>
         <div class="text-box">
-            <div style="color:#3478f6; font-size:16px; font-weight:600; margin-bottom:10px;">전국 등록 현황</div>
-            <div style="font-size:30px; font-weight:700; line-height:1.5;">
+            <div style="color:#3478f6; font-size:30px; font-weight:600; margin-bottom:30px;">전국 등록 현황</div>
+            <div style="font-size:20px; font-weight:700; line-height:1.5;">
                 지역별 등록 현황을<br>
                 차종과 지역 특성에 따라 비교해보세요.
             </div>
